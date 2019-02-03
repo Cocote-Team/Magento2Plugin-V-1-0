fa6daef95f5e8d2921bee94a4b1d8ca116957bdd
Jakub <jakilcz@gmail.com>
2019-02-04 00:20:27 +0100

    v 1.2.1 - order tracking by js

---
+++ Block/Success.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Cocote\Feed\Block;
+
+use Magento\Customer\Model\Context;
+use Magento\Sales\Model\Order;
+
+/**
+ * One page checkout success page
+ *
+ * @api
+ */
+class Success extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * @var \Magento\Sales\Model\Order\Config
+     */
+    protected $_orderConfig;
+
+    /**
+     * @var \Magento\Framework\App\Http\Context
+     */
+    protected $httpContext;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Sales\Model\Order\Config $orderConfig
+     * @param \Magento\Framework\App\Http\Context $httpContext
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Sales\Model\Order\Config $orderConfig,
+        \Magento\Framework\App\Http\Context $httpContext,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_checkoutSession = $checkoutSession;
+        $this->_orderConfig = $orderConfig;
+        $this->_isScopePrivate = true;
+        $this->httpContext = $httpContext;
+    }
+
+    public function getOrder()
+    {
+        $order = $this->_checkoutSession->getLastRealOrder();
+        return $order;
+    }
+    
+    public function getOrderTotal()
+    {
+        $order = $this->_checkoutSession->getLastRealOrder();
+        return $order->getGrandTotal();
+    }
+
+}
